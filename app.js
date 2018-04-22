698847369c9b5d9b35228f48618ca872f960dedf
root <root@Norton.localdomain>
2018-04-22 11:38:20 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+let fs = require('fs');
+
+fs.readFile('./leads.json', 'utf8', function(err, data) {
+	if (err) {
+		console.log(`Error occured: ${err}`);
+	}
+	data = JSON.parse(data);
+	processData(data.leads);
+})
+
+const writeChangelog = (changes) => {
+	changes.forEach(change => {
+		fs.appendFile('./changelog.txt', `${change}\r\n`, (err) => {
+			if (err) {
+				console.log(`Error appending to file: ${err}`);
+			}
+			console.log(`Wrote ${change} to changelog.`)
+		})	
+	})
+}
+
+const findDuplicates = (records) => {
+	let duplicates = {};
+	records.forEach((record, recordIndex) => {
+		let currentDupes = records.filter((recordToCheck, filterIndex) => {
+			if (recordIndex !== filterIndex) {
+				return (recordToCheck['_id'] === record['_id'] || recordToCheck['email'] === record['email'])
+			}
+		})
+		duplicates[recordIndex] = currentDupes;
+	})
+	console.log(duplicates); // remove this little guy
+	return duplicates
+}
+
+const sortByTimestamp = (records) => {
+	return records.sort((first, second) => {
+		let firstDate = new Date(first.entryDate).getTime();
+		let secondDate = new Date(second.entryDate).getTime();
+		return firstDate > secondDate ? -1 : 1
+	})
+}
+
+const processData = (records) => {
+	let cleanedData = {
+		'leads': []
+	};
+	findDuplicates(records);
+	let testsWrites = ['changed this to that', 'changed that to this', 'another this that swap'];
+
+	writeChangelog(testsWrites)
+}
+
+
+// iterate through each record grabbing duplicates by ID and email
+// order duplicates by timestamp (oldest to newest)
+// check name + address for each duplicate
+	// if matches
+		// use most recent version of data
+	// if doesn't match
+		// change ID field (email can't be reasonably changed) --> how to handle email conflicts? John Smith + Micah Valmer same email + address
+
+		
