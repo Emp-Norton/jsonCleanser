06f9e866402a8c7bab8b86bdd3af53376b3b7f56
emp-norton <jamesjelenko@gmail.com>
2018-04-23 15:57:24 -0700

    add function to identify unique records, remove comments"

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+'use strict';
+
 let fs = require('fs');
 
 fs.readFile('./leads.json', 'utf8', function(err, data) {
 	if (err) {
-		console.log(`Error occured: ${err}`);
+		console.log(`Error occured reading from filter: ${err}`);
 	}
+
 	data = JSON.parse(data);
 	processData(data.leads);
 })
@@ -14,47 +17,57 @@ const writeChangelog = (changes) => {
 			if (err) {
 				console.log(`Error appending to file: ${err}`);
 			}
-			console.log(`Wrote ${change} to changelog.`)
+
+			console.log(`Wrote ${change} to changelog.`);
 		})	
 	})
 }
 
-const findDuplicates = (records) => {
+const findDuplicatesByEmail = (records) => {
 	let duplicates = {};
 	records.forEach((record, recordIndex) => {
 		let currentDupes = records.filter((recordToCheck, filterIndex) => {
-			if (recordIndex !== filterIndex) {
-				return (recordToCheck['_id'] === record['_id'] || recordToCheck['email'] === record['email'])
-			}
+			return recordToCheck['email'] === record['email']
 		})
-		duplicates[recordIndex] = currentDupes;
+
+		duplicates[record['email']] = sortByTimestamp(currentDupes);
 	})
-	console.log(duplicates); // remove this little guy
+
 	return duplicates
 }
 
 const sortByTimestamp = (records) => {
+	
 	return records.sort((first, second) => {
 		let firstDate = new Date(first.entryDate).getTime();
 		let secondDate = new Date(second.entryDate).getTime();
+		
 		return firstDate > secondDate ? -1 : 1
 	})
 }
 
+const getUniqueAddresses = (emailRecords) => {
+	let uniqueRecords = [];
+
+	for (let record in emailRecords) {
+		if (emailRecords[record].length === 1) {
+			uniqueRecords.push(emailRecords[record][0]);
+		}
+	}
+
+	return uniqueRecords
+}
+
 const processData = (records) => {
-	let cleanedData = {
+	let cleanData = {
 		'leads': []
 	};
-	findDuplicates(records);
+
+	let recordsByEmail = findDuplicatesByEmail(records);
+	let uniqueRecords = getUniqueAddresses(recordsByEmail);
+	
+	
+	console.log(recordsByEmail)
 }
 
 
-// iterate through each record grabbing duplicates by ID and email
-// order duplicates by timestamp (oldest to newest)
-// check name + address for each duplicate
-	// if matches
-		// use most recent version of data
-	// if doesn't match
-		// change ID field (email can't be reasonably changed) --> how to handle email conflicts? John Smith + Micah Valmer same email + address
-
-		
